fbe9804713579737b91ec7ca303e593210ee01d9
snallbati19 <snallbati19@epoka.edu.al>
2024-06-21 13:30:05 +0200

    changes

--- server.js
+++ server.js
@@ -9,13 +9,17 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, apiurl'); // Add 'apiurl' header
-  next();
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200); // Respond to OPTIONS requests with HTTP OK status
+  } else {
+    next();
+  }
 });
 
 // Route to proxy server
 app.get('/proxyserver', async (req, res) => {
   try {
-    const apiUrl = req.headers.apiurl; 
+    const apiUrl = req.headers.apiurl;
     const response = await fetch(apiUrl, {
       headers: {
         Authorization: req.headers.authorization
